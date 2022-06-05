0bbfb355170b3385b25b8288a56303a702a36f19
Robert Peterman <robpeterman573@gmail.com>
2022-06-04 23:00:46 -0400

    added body parsing

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post("/test", (req, res) => {
-console.log("endpoint hit", req);
+console.log("endpoint hit", req.body);
   res.send("Hello World!");
 });
 
